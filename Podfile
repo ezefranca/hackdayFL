22f0e526a5395c93d3998bb34ae9c607429fc30a
ezefranca <ezequiel.ifsp@gmail.com>
2015-03-21 18:47:11 -0300

    first comia

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+inhibit_all_warnings!
+
+platform :ios, '8.0'
+
+pod 'PNChart'
+pod 'TEAChart'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// PNChart
+#define COCOAPODS_POD_AVAILABLE_PNChart
+#define COCOAPODS_VERSION_MAJOR_PNChart 0
+#define COCOAPODS_VERSION_MINOR_PNChart 8
+#define COCOAPODS_VERSION_PATCH_PNChart 2
+
+// TEAChart
+#define COCOAPODS_POD_AVAILABLE_TEAChart
+#define COCOAPODS_VERSION_MAJOR_TEAChart 0
+#define COCOAPODS_VERSION_MINOR_TEAChart 3
+#define COCOAPODS_VERSION_PATCH_TEAChart 1
+
+// UICountingLabel
+#define COCOAPODS_POD_AVAILABLE_UICountingLabel
+#define COCOAPODS_VERSION_MAJOR_UICountingLabel 1
+#define COCOAPODS_VERSION_MINOR_UICountingLabel 0
+#define COCOAPODS_VERSION_PATCH_UICountingLabel 0
+
